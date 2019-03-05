be81f89b152474a0e11ad7cff7088d938dca4648
Paul Meier <paul@fiterr.com>
2019-03-05 14:57:18 +1100

    Started Flavour Types Class

---
+++ Flavour_types.rb
@@ -0,0 +1,31 @@
+# class FlavourTypes
+#     #
+#     def initialize(category_name, cuisines)
+#         @category_name = make
+#         @cuisines = year
+#     end
+# end
+
+# #  INSTANCES
+# #  1. Hot & Spicy  2. Creamy & Rich  3. Mild  and Fresh
+
+# eg_flavour_type = {
+#     category_name: '1. Hot & Spicy',
+#     cuisines: ['Indian', 'Chinese']
+# }
+
+@a = 5
+
+# if @a > 10
+#     my_var = "Hi"
+# else
+#     my_var = "Bye"
+# end
+
+def puts_thing
+    puts @a
+end
+
+# puts my_var
+
+puts_thing
