d113c3beff1c0dca1663cfdf10db8247f1549248
Gwenny Warnick <gwennywarnick@Gwennys-MacBook-Air.local>
2019-03-06 11:17:55 +1100

    Adding updated code and screenshot of current Trello board status

--- RestaurantClassDRAFT.rb
+++ RestaurantClassDRAFT.rb
@@ -9,51 +9,61 @@ require 'csv'
 require 'pry'
 
 def read_line_by_line(filename)
+    array_of_restaurants = []
     CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
         name = row[2].strip
         address = row[0].strip
-        puts name
-        puts address
-        # address = row[:Address]
-        # #must pull from other files as well as csv—include here?:
-        # cuisine = row[:Cuisine]
-        # flavour = row[:Flavour]
-		# puts "#{name}, #{address}, #{flavour}"
-	end
+        cuisine_type = row[3].strip
+        flavour_type = row[4].strip
+
+        restaurant_hash = {
+            name: name,
+            address: address,
+            cuisine_type: cuisine_type,
+            flavour_type: flavour_type 
+        }
+
+        array_of_restaurants << restaurant_hash
+    end
+    return array_of_restaurants
 end
 
 def read_all_at_once(filename)
     puts CSV.read(filename)
 end
 
-read_line_by_line(filepath)
-# read_all_at_once(Restaurant_list)
+# method to find all of the restaurants from the cuisine that the user selects, which will return an array of restaurants by cuisine
+def create_array_of_restaurants_by_cuisine(filepath)
+    array_of_restaurants_by_cuisine = []
+    array_of_restaurants = read_line_by_line(filepath)
+    restaurants_by_cuisine_type = array_of_restaurants.select do |restaurant|
+        restaurant[:cuisine_type] == "Vietnamese"
+    end
+    return restaurants_by_cuisine_type
+end
 
 
-# class Restaurant 
-#     #allow getters for name and address 
-#     attr_reader :name, :address
-#     # initalise class attributes
-#     def initialize(name, address, cuisine_type, flavour_type)
-#         #instance variables
-#         @name = name
-#         @address = address
-#         #instance variables in other classes (FlavourType, )
-#         @cuisine_type = cuisine_type
-#         @flavour_type = flavour_type
-#     end 
+# method to go through array of restaurants by cuisine and return random selection (method .sample)
+def make_restaurant_recommendation(filepath)
+    restaurant_recommendation = create_array_of_restaurants_by_cuisine(filepath) 
+    recommendation = restaurant_recommendation.sample
+    final_string = """
+    We recommend this restaurant for you! 
+    Cuisine: #{recommendation[:cuisine_type]}
+    Restaurant: #{recommendation[:name]}
+    Address: #{recommendation[:address]}
 
-#     #method to suggest restaurant based on cuisine type (with cuisine type coming from cuisine class)
-#     def recommend_restaurant_from_cuisine_type
-#         cuisine_type() #GW note: check return is a cuisine type
-#         #based on cuisine type, push out a random choice of restaurant from the restaurant array that cuisine type category 
-        
-#         return restaurant_suggestion
-  
-#     end
-# end
+    Sounds great!     Press 1
+    Try again please. Press 2
+    """
+    return final_string
+end
 
+puts make_restaurant_recommendation(filepath)
+# p create_array_of_restaurants_by_cuisine(filepath)
 
+#Anhar help pls! 
+# restaurant_1 = Restaurant.new("#{@name}")
 # rest_one = Restaurant.new("La Porchetta", "300 Lonsdale St, Melbourne 3000", "Italian", "mild and fresh")
 # rest_two = Restaurant.new("La Porchetta", "300 Lonsdale St, Melbourne 3000", "Italian", "mild and fresh")
 # rest_three = Restaurant.new("La Porchetta", "300 Lonsdale St, Melbourne 3000", "Italian", "mild and fresh")
